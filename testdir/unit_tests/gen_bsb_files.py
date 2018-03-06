9cc0fa505650f86b4e7fc705e714eb07b23ea930
steveri <steveri@steveri.com>
2018-03-06 10:48:22 -0800

    mothballing this for awhile

--- testdir/unit_tests/gen_bsb_files.py
+++ testdir/unit_tests/gen_bsb_files.py
@@ -214,32 +214,110 @@ if os.getenv('NO_IO_TILES'):
     OP_TEMPLATE  =  OP_TEMPLATE_OLD
 
 
-# So what we gonna do is...
-# we gonna modify top_tb to send one-bit outputs
-# from pre-specified output wire like maybe "T4_out_s2t0.1",
-# to a separate output file something like CGRA_1bit.out
+# # So what we gonna do is...
+# # we gonna modify top_tb to send one-bit outputs
+# # from pre-specified output wire like maybe "T4_out_s2t0.1",
+# # to a separate output file something like CGRA_1bit.out
+# 
+# # Use PEs to process input pixel(s) for LUT
+# # Use EQ function
+# LUT_TEMPLATE='''
+#   # Use EQ to turn 0/1 pixels and turn them into 0/1 bits on tracks 0,1,2
+#   self.in -> T11_in_s2t0
+#   T11_in_s2t0 -> T11_op1
+#   T11_eq(const_1, wire)
+#   T11_pe_out_res_p -> T11_out_s0t0.1
+# 
+#   T12_bit0 <- T12_in_s2t0
+#   T12_bit3 <- T12_in_s2t0 (r)
+#   T12_LUT(wire,const_0,reg) <- 0x01
+#   T12_pe_out_res_p -> T12_out_s1t0.1
+# '''
+# 
+# # Simple one to get us started
+# LUT_TEMPLATE='''
+#   T12_LUT(wire,const,reg) <- 0x01
+#   T4_LUT <- 0xFF
+#   T4_pe_out_res_p -> T4_out_s2t0.1
+# '''
 
-# Use PEs to process input pixel(s) for LUT
-# Use EQ function
-LUT_TEMPLATE='''
-  # First we bring in 0/1 pixels and turn them into 0/1 bits
-  self.in -> T11_in_s2t0
-  T11_in_s2t0 -> T11_op1
-  T11_eq(const_1, wire)
-  T11_pe_out_res_p -> T11_out_s0t0.1
+# NOPE start over
+# * Set LUT registers for 3-input XOR maybe
+# * If everything works right, a 0 pixel on the input should
+#   translate to BUS1_S2_T0 single-bit signals on the first 3 PE tiles
+# * Must route these to e.g. the three LUT inputs of the central PE
+# * DON'T NEED/WANT EQ TRANSLATION!!! Ugh.
+#
+# 3-input AND looks like: 8'b10000000
+# To load LUT with AND function: 0000TTTT 10000000
+#
+# Three LSB bits come in on e7, f5, 107 tracks 0,1,2 respectively
 
-  T12_bit0 <- T12_in_s2t0
-  T12_bit3 <- T12_in_s2t0 (r)
-  T12_LUT(wire,const_0,reg) <- 0x01
-  T12_pe_out_res_p -> T12_out_s1t0.1
-'''
+#  NOTE bit0<-S2 bit1<-S1, bit2<-S2
 
-# Simple one to get us started
-LUT_TEMPLATE='''
-  T12_LUT(wire,const,reg) <- 0x01
-  T4_LUT <- 0xFF
-  T4_pe_out_res_p -> T4_out_s2t0.1
-'''
+
+# 0 ...... ......  io1_02 io1_03 
+# 1 ...... ......  io16_12 ......
+# 2 io1_13 io16_14 pe_15  pe_16  
+# 3 io1_27 ......  pe_28  pe_29  
+# 4 io1_35 ......  pe_36  pe_37  
+# 0 io1_47 ......  pe_48  pe_49  
+# 1 io1_55 ......  pe_56  pe_57  
+# 2 io1_67 ......  pe_68  pe_69  
+# 3 io1_75 ......  pe_76  pe_77  
+# 4 io1_87 ......  pe_88  pe_89  
+# 0 io1_95 ......  pe_96  pe_97  
+# 1 io1_A7 ......  pe_A8  pe_A9  
+# 2 io1_B5 ......  pe_B6  pe_B7  
+# 3 io1_C7 ......  pe_C8  pe_C9  
+# 4 io1_D5 ......  pe_D6  pe_D7  
+# 0 io1_E7 ......  pe_E8  pe_E9  
+# 1 io1_F5 ......  pe_F6  pe_F7  
+# 2 io1_107 ......  pe_108 pe_109 
+#   ...... ......  io16_115 ......
+#   ...... ......  io1_116 io1_117
+#   
+# 
+# LUT_TEMPLATE='''
+#   #DELAY 0,0
+#   #
+#   self.in ->  T0xE8_in_s2t0 ->  T0xE8_out_s1t0 -> T0xE8_bit2
+#   self.in ->  T0xF6_in_s2t1 ->  T0xF6_out_s3t1 -> T0xE8_bit1
+#   self.in -> T0x108_in_s2t2 -> T0x108_out_s3t2 -> T0xF6_out_s3t2 -> T0xE8_bit0
+#   #
+#   
+# 
+# 
+# 
+# 
+#   self.in -> T21_in_s2t0
+#   T21_in_s2t0 -> T21_op1
+#   T21_in_s2t0 -> T21_out_s1t0
+#   T21_out_s1t0 -> T21_op2 (r)
+#   T21_OPNAME(wire,reg)
+#   T21_pe_out -> T21_out_s0t0
+#   #
+#   T22_in_s2t0 -> T22_out_s0t0
+#   T23_in_s2t0 -> T23_out_s0t0
+#   T24_in_s2t0 -> T24_out_s0t0
+#   #
+#   T25_in_s2t0 -> T25_out_s0t0
+#   T26_in_s2t0 -> T26_out_s0t0
+#   T27_in_s2t0 -> T27_out_s0t0
+#   T28_in_s2t0 -> T28_out_s0t0
+#   #
+#   T29_in_s2t0 -> T29_out_s0t0
+#   T30_in_s2t0 -> T30_out_s0t0
+#   T31_in_s2t0 -> T31_out_s0t0
+#   T32_in_s2t0 -> T32_out_s0t0
+#   #
+#   T33_in_s2t0 -> T33_out_s0t0
+#   T34_in_s2t0 -> T34_out_s0t0
+#   T35_in_s2t0 -> T35_out_s0t0
+#   T36_in_s2t0 -> T36_out_s0t0 -> self.out
+# '''
+
+
 
 
 
