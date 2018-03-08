a962ba35b5bda795873e3c05839b540c04821e14
Lenny Truong <lenny@stanford.edu>
2018-03-07 22:39:50 -0800

    Add more tests for pe

--- tests/test_pe/test_pe.py
+++ tests/test_pe/test_pe.py
@@ -1,19 +1,49 @@
+import pytest
 import subprocess
-from pe import abs, and_
+import pe
 from testvectors import complete
 from verilator import compile, run_verilator_test
 
-def test_abs():
-    a = abs()
+ops  = ['or_', 'and_', 'xor']
+# ops += ['inv']
+ops += ['lshr', 'lshl']
+# ops += ['ashr']
+ops += ['add', 'sub']
+ops += ['abs']
+ops += ['eq']
+ops += ['sel']
+
+signed_ops = ['min', 'max', 'le', 'ge']
+def pytest_generate_tests(metafunc):
+    if 'op' in metafunc.fixturenames:
+        metafunc.parametrize("op", ops)
+    if 'signed_op' in metafunc.fixturenames:
+        metafunc.parametrize("signed_op", signed_ops)
+        metafunc.parametrize("signed", [True, False])
+    if 'const_value' in metafunc.fixturenames:
+        metafunc.parametrize("const_value", range(16))
+
+def test_op(op):
+    a = getattr(pe, op)()
 
     tests = complete(a, 4, 16)
 
-    compile('test_abs_complete', 'test_pe_comp_unq1', a.opcode, tests)
-    run_verilator_test('test_pe_comp_unq1', 'sim_test_abs_complete', 'test_pe_comp_unq1')
+    compile(f'test_{op}_complete', 'test_pe_comp_unq1', a.opcode, tests)
+    run_verilator_test('test_pe_comp_unq1', f'sim_test_{op}_complete', 'test_pe_comp_unq1')
 
-def test_and():
-    a = and_()
+def test_signed_op(signed_op, signed):
+    a = getattr(pe, signed_op)(signed)
 
     tests = complete(a, 4, 16)
-    compile('test_and_complete', 'test_pe_comp_unq1', a.opcode, tests)
-    run_verilator_test('test_pe_comp_unq1', 'sim_test_and_complete', 'test_pe_comp_unq1')
+
+    compile(f'test_{signed_op}_complete', 'test_pe_comp_unq1', a.opcode, tests)
+    run_verilator_test('test_pe_comp_unq1', f'sim_test_{signed_op}_complete', 'test_pe_comp_unq1')
+
+@pytest.mark.skip
+def test_const(const_value):
+    a = pe.const(const_value)
+
+    tests = complete(a, 4, 16)
+
+    compile(f'test_const_complete', 'test_pe_comp_unq1', a.opcode, tests)
+    run_verilator_test('test_pe_comp_unq1', f'sim_test_const_complete', 'test_pe_comp_unq1')
